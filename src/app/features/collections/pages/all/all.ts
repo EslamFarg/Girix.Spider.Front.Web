78c7572bc9ed3134bca6bae602a5a5372eace2ca
yousofo <jon772002@gmail.com>
2025-12-21 16:57:36 +0200

    added collection dialog boilerplate

--- src/app/features/collections/pages/all/all.ts
+++ src/app/features/collections/pages/all/all.ts
@@ -1,40 +1,60 @@
 import { BaseComponent } from '@/components/base-component/base-component';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
-import { SectionWrapper } from "@/components/section-wrapper/section-wrapper";
-import { InputErrorMessageHandler } from "@/components/input-error-message-handler/input-error-message-handler";
-import { InputGroupAddon } from "primeng/inputgroupaddon";
-import { Select } from "primeng/select";
-import { Paginator } from "primeng/paginator";
-import { InputText } from "primeng/inputtext";
+import { SectionWrapper } from '@/components/section-wrapper/section-wrapper';
+import { InputErrorMessageHandler } from '@/components/input-error-message-handler/input-error-message-handler';
+import { InputGroupAddon } from 'primeng/inputgroupaddon';
+import { Select } from 'primeng/select';
+import { Paginator } from 'primeng/paginator';
+import { InputText } from 'primeng/inputtext';
+import { Dialog } from 'primeng/dialog';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-all',
-  imports: [SectionWrapper, InputErrorMessageHandler, InputGroupAddon, Select, Paginator, ReactiveFormsModule, InputText],
+  imports: [
+    SectionWrapper,
+    InputErrorMessageHandler,
+    InputGroupAddon,
+    Select,
+    Paginator,
+    ReactiveFormsModule,
+    InputText,
+    Dialog,
+    Button
+],
   templateUrl: './all.html',
   styleUrl: './all.css',
 })
-export class All   extends BaseComponent {
+export class All extends BaseComponent {
   initialSearchFormValue = {
     text: this.fb.control<string>('', [Validators.required]),
     categoryId: this.fb.control<number>(0, [Validators.required]),
   };
   fg = this.fb.group(this.initialSearchFormValue);
 
-
-
-  periodOptions=[
-    {label:'اليوم',value:1},
-    {label:'الاسبوع',value:2},
-    {label:'الشهر',value:3},
-    {label:'السنة',value:4},
-  ]
+  periodOptions = [
+    { label: 'اليوم', value: 1 },
+    { label: 'الاسبوع', value: 2 },
+    { label: 'الشهر', value: 3 },
+    { label: 'السنة', value: 4 },
+  ];
 
   onSubmit() {}
-
 
   first = 0;
   rows = 10;
   onPageChange(event: any) {}
 
+
+  isCollectionInvoiceDialogVisible = false;
+
+
+  openCollectionInvoiceDialog() {
+    this.isCollectionInvoiceDialogVisible = true;
+  }
+
+  closeCollectionInvoiceDialog() {
+    this.isCollectionInvoiceDialogVisible = false;
+  }
 }
